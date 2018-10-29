5aaa185946f26cb262a202257ccf945ffab2ef6e
Pedro Lucas <pedrolucas499@gmail.com>
2018-10-29 14:12:31 -0300

    Produto Detalhes + Data Initializer (INSERTS AUTOMATICO) + Melhorias

--- src/main/java/com/ecommerce/redes/controllers/IndexController.java
+++ src/main/java/com/ecommerce/redes/controllers/IndexController.java
@@ -1,10 +1,14 @@
 package com.ecommerce.redes.controllers;
 
+import com.ecommerce.redes.models.Produto;
 import com.ecommerce.redes.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -14,9 +18,22 @@ public class IndexController {
 
     @RequestMapping(value = "/")
     public String index(Model model){
-        model.addAttribute("produtos", ps.getAll());
-        model.addAttribute("principal", ps.getAll().get(2));
+
+        Produto banner_principal = ps.getOne(1);
+
+        List<Produto> produtos_list = ps.getAll();
+        produtos_list.remove(banner_principal);
+
+        model.addAttribute("produtos", produtos_list);
+        model.addAttribute("principal", banner_principal);
+
         return "index";
+    }
+
+    @RequestMapping(value = "/produto/{id}")
+    public String produto_detalhe(Model model, @PathVariable("id") Integer id){
+        model.addAttribute("produto", ps.getOne(id));
+        return "produto_detalhe";
     }
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+delete from tab_produto;
+
+insert into public.tab_produto(
+            id, descricao, nome, path_img, preco)
+    values (1, 'Ultimo lançamento da Apple', 'IPhone X', 'https://dc7z0p3u9b23o.cloudfront.net/uploads/iphone-x_chamada1.jpg', 5800);
+
+insert into public.tab_produto(
+            id, descricao, nome, path_img, preco)
+    values (2, 'Placa de NVIDIA 11GB', 'RTX 2080TI', 'https://images.idgesg.net/images/article/2018/08/geforce-rtx-2080-ti-long-100769235-large.jpg', 12200);
+
+insert into public.tab_produto(
+            id, descricao, nome, path_img, preco)
+    values (3, 'Memoria RAM Kingston', 'HyperX 8GB RGB', 'https://www.oficinadanet.com.br/imagens/post/21534/capa02.jpg', 950);
+
+insert into public.tab_produto(
+	id, descricao, nome, path_img, preco)
+    values (4, 'Mouse Razer', 'Mamba Elite', 'https://www.tibahia.com/upload/images/2018-07-19/20180719055956_Razer-Mamba-Elite--2018--Neon-Concept-2.png' , 540);
